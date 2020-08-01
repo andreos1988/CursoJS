43e0561a9d772537cba325d778e0f3236d511c27
André Oliveira <andreprogramador10@gmail.com>
2020-08-01 14:20:51 -0300

    atualizacao de projeto

---
+++ vue/most-used-words/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-tile>Most Used Words</v-toolbar-tile>  
+    </v-app-bar>
+    <v-content>
+      <Home />
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+   Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ vue/most-used-words/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <v-container fluid>
+            <v-form >
+                <v-file-input label="Selecione as legendas"
+                            prepend-icon="mdi-message-text"
+                            append-outer-icon="mdi-send"
+                            outlined
+                            multilple 
+                            chips 
+                            v-model="files"
+                            @click:append-outer="processSubtitles">
+
+                </v-file-input>
+            </v-form>
+            <div class="pills">
+                <Pill v-for="word in groupedWords" :key="word.name"
+                    :name="word.name" :amount="word.amount" />
+            </div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data: function(){
+        return {
+            files: [],
+            groupedWords: [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },
+                
+            ]
+        }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style>
+    .pills{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+</style>
